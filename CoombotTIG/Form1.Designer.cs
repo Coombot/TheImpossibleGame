67192084f7d00bb91a302360f77bc7bdac07bad3
Kiffolisk <45130240+Kiffolisk@users.noreply.github.com>
2021-06-26 18:29:58 +0200

    Source code

---
+++ CoombotTIG/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace CoombotTIG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lookForGame1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lookForGame1
+            // 
+            this.lookForGame1.AutoSize = true;
+            this.lookForGame1.Location = new System.Drawing.Point(12, 286);
+            this.lookForGame1.Name = "lookForGame1";
+            this.lookForGame1.Size = new System.Drawing.Size(141, 13);
+            this.lookForGame1.TabIndex = 0;
+            this.lookForGame1.Text = "Looking for game instance...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(532, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Macro name here";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(426, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Press F2 to start recording. Press F10 to stop recording. Press F7 to play back t" +
+    "he macro.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 308);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lookForGame1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lookForGame1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CoombotTIG/Form1.cs
@@ -0,0 +1,151 @@
+﻿using EventHook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoombotTIG
+{
+    public partial class Form1 : Form
+    {
+        // Game
+        public static IntPtr BaseAddress;
+        public static VAMemory GameMemory;
+        public static Process GameProcess;
+        public static bool GameHooked = false;
+
+        // Hooks
+        public static EventHookFactory eventHookFactory = new EventHookFactory();
+        public static MouseWatcher mouseWatcher = eventHookFactory.GetMouseWatcher();
+
+        public static Thread recordPlayThread;
+
+        // Useful Bot Variables
+        public static bool IsPaused
+        {
+            get
+            {
+                return false; 
+            }
+        }
+        public static int CurFrame
+        {
+            get
+            {
+                IntPtr timerAddress = BaseAddress + 0x59518;
+                return GameMemory.ReadInt32(timerAddress);
+            }
+            set
+            {
+                IntPtr timerAddress = BaseAddress + 0x59518;
+                GameMemory.WriteInt32(timerAddress, value);
+            }
+        }
+
+        // Bot Bools
+        public static bool isRecording = false;
+        public static bool isPlayingBack = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static bool IsArrayNullOrEmpty<T>(T[] array) where T : class
+        {
+            if (array == null || array.Length == 0)
+                return true;
+            else
+                return array.All(item => item == null);
+        }
+
+        public static void Playback()
+        {
+
+        }
+
+        public static void StartRecording(string macroName)
+        {
+            using (var eventHookFactory = new EventHookFactory())
+            {
+                // need to implement save reading
+                if (!Directory.Exists($@"C:\Users\{Environment.UserName}\AppData\Local\Coombot TIG Macros"))
+                {
+                    Directory.CreateDirectory($@"C:\Users\{Environment.UserName}\AppData\Local\Coombot TIG Macros");
+                }
+                string MacroDirectory = $@"C:\Users\{Environment.UserName}\AppData\Local\Coombot TIG Macros";
+                File.Create($@"{MacroDirectory}\{macroName}.cbt").Close();
+                CurFrame = 0;
+                File.WriteAllText($@"{MacroDirectory}\{macroName}.cbt", $"-- COOMBOT FOR THE IMPOSSIBLE GAME MACRO --");
+
+                mouseWatcher.Start();
+                mouseWatcher.OnMouseInput += (s, e) =>
+                {
+                    // DEFAULT TO LBUTTON, NEEDS TO IMPLEMENT SAVE READING
+                    if (e.Message.ToString().Trim() == "WM_LBUTTONDOWN")
+                    {
+                        string MacroText = File.ReadAllText($@"{MacroDirectory}\{macroName}.cbt");
+                        File.WriteAllText($@"{MacroDirectory}\{macroName}.cbt", MacroText + $"\ninputType:1 timerFrame:{CurFrame} x:{e.Point.x}, y:{e.Point.y}");
+                    }
+                    else if (e.Message.ToString().Trim() == "WM_LBUTTONUP")
+                    {
+                        string MacroText = File.ReadAllText($@"{MacroDirectory}\{macroName}.cbt");
+                        File.WriteAllText($@"{MacroDirectory}\{macroName}.cbt", MacroText + $"\ninputType:0 timerFrame:{CurFrame} x:{e.Point.x}, y:{e.Point.y}");
+                    }
+                };
+            }
+        }
+
+        public static void StopRecording(string macroName)
+        {
+            mouseWatcher.Stop();
+        }
+
+        public void LookForGame()
+        {
+            lookForGame1.Text = "Looking for game instance...";
+            if (!IsArrayNullOrEmpty(Process.GetProcessesByName("ImpossibleGame")))
+            {
+                GameProcess = Process.GetProcessesByName("ImpossibleGame").FirstOrDefault();
+                GameMemory = new VAMemory("ImpossibleGame");
+
+                BaseAddress = GameProcess.MainModule.BaseAddress;
+
+                GameHooked = true;
+                lookForGame1.Text = "Game instanced attached!";
+            }
+            else
+            {
+                lookForGame1.Text = "No game instance found.";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LookForGame();
+
+            // inputting
+            var keyboardWatcher = eventHookFactory.GetKeyboardWatcher();
+            keyboardWatcher.Start();
+            keyboardWatcher.OnKeyInput += (s, ev) =>
+            {
+                if (ev.KeyData.Keyname == "F2")
+                {
+                    StartRecording(textBox1.Text);
+                }
+                if (ev.KeyData.Keyname == "F10")
+                {
+                    StopRecording(textBox1.Text);
+                }
+                Console.WriteLine(string.Format("Key {0} event of key {1}", ev.KeyData.EventType, ev.KeyData.Keyname));
+            };
+        }
+    }
+}
